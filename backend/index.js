68eb9ebf79908f978eea37332bff73a7ed76d6a7
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-11-02 21:37:46 +0530

    setting up backend

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express, { urlencoded } from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import 'dotenv/config'
+import connectDB from './utils/db.js'
+
+
+
+const app = express()
+
+app.get("/",(req,res)=>{
+    return res.status(200).json({
+        message:"I'm coming from backend",
+        success:true
+    })
+})
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(urlencoded({extended:true}))
+
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true
+}
+
+app.use(cors(corsOptions))
+
+const PORT = process.env.PORT
+
+
+
+app.listen(PORT,()=>{
+    connectDB()
+    console.log(`Server is running at port ${PORT}`)
+})
+
+
