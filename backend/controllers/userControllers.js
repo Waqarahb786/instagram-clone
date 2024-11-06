19559ae48469ac6759101f319e8f566098587aef
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-11-06 21:39:42 +0530

    testing user api through postman and fixed some bugs

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -129,7 +129,7 @@ export const editProfile = async (req,res)=>{
           cloudResponse = await cloudinary.uploader.upload(fileUri)
         }
 
-        const user = await User.findById(userId);
+        const user = await User.findById(userId).select("-password");
         if(!user){
           return res.status(404).json({
             message:'User not found',
