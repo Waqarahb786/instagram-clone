fed7afbe2afe8228964fc91d01257c106dfad217
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-11-07 18:14:20 +0530

    getUserPost

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -62,4 +62,23 @@ export const getAllPost = async (req,res)=>{
     }
 }
 
-ex
+export const getUserPost = async(req,res)=>{
+    try {
+        const authorId=req.id
+        const posts=await Post.find({author:authorId}).sort({createdAt:-1}).populate({path:'author',select:'username, profilePicture'})
+        .populate({
+            path:'comments',
+            sort:{createdAt:-1},
+            populate:{
+                path:'author',
+                select:'username, profilePicture'
+            }
+                });
+                return res.status(200).json({
+                    posts,
+                    success:true
+                })
+    } catch (error) {
+        console.log(error,"getUserPost api error")
+    }
+}
