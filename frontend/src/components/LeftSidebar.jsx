f26bf236db93f7c229af8aa0fc4b9b71ae982a1e
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-11-14 08:25:33 +0530

    redux toolkit integration completed

--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -11,32 +11,20 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { toast } from "sonner";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setAuthUser } from "@/redux/authSlice";
 
-const sidebarItems = [
-  { icon: <Home />, text: "Home" },
-  { icon: <Search />, text: "Search" },
-  { icon: <TrendingUp />, text: "Explore" },
-  { icon: <MessageCircle />, text: "Messages" },
-  { icon: <Heart />, text: "Notification" },
-  { icon: <PlusSquare />, text: "Create" },
-  {
-    icon: (
-      <Avatar className="w-6 h-6">
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-    ),
-    text: "Profile",
-  },
-  { icon: <LogOut />, text: "Logout" },
-];
+
 
 const LeftSidebar = () => {
     const navigate = useNavigate()
+    const {user} = useSelector(store=>store.auth)
+    const dispatch = useDispatch()
     const logoutHandler = async()=>{
         try {
             const res = await axios.get('http://localhost:8000/api/v1/user/logout',{withCredentials:true})
             if(res.data.success){
+                dispatch(setAuthUser(null))
                 navigate('/login')
                 toast.success(res.data.message)
             }
@@ -47,6 +35,25 @@ const LeftSidebar = () => {
     const sidebarHandler=(textType)=>{
         if(textType === 'Logout') logoutHandler()
     }
+
+    const sidebarItems = [
+      { icon: <Home />, text: "Home" },
+      { icon: <Search />, text: "Search" },
+      { icon: <TrendingUp />, text: "Explore" },
+      { icon: <MessageCircle />, text: "Messages" },
+      { icon: <Heart />, text: "Notification" },
+      { icon: <PlusSquare />, text: "Create" },
+      {
+        icon: (
+          <Avatar className="w-6 h-6">
+            <AvatarImage src={user?.profilePicture} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        ),
+        text: "Profile",
+      },
+      { icon: <LogOut />, text: "Logout" },
+    ];
   return (
   <div className="fixed top-0 z-10 left-0 px-4 border-r border-gray-300 w-[16%] h-screen">
     <div className="flex flex-col">
