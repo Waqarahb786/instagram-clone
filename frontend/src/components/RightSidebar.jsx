066fb850bfd6b6df81c1750119d4c19b0c160b52
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-11-18 11:07:28 +0530

    designing right side bar

--- frontend/src/components/RightSidebar.jsx
+++ frontend/src/components/RightSidebar.jsx
@@ -1,9 +1,32 @@
-
+import { useSelector } from "react-redux";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import store from "@/redux/store";
+import { Link } from "react-router-dom";
 
 const RightSidebar = () => {
+  const { user } = useSelector((store) => store.auth);
   return (
-    <div>RightSidebar</div>
-  )
-}
+    <div className="w-fit my-10 pr-32">
+      <div className="flex items-center gap-2">
+        <Link to={`profile/${user?._id}`}>
+          <Avatar>
+            <AvatarImage src={user?.profilePicture} alt="post_image" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </Link>
 
-export default RightSidebar
+        <div>
+          <Link to={`profile/${user?._id}`}>
+            <h1 className="font-semibold text-sm">{user?.username}</h1>
+          </Link>
+
+          <span className="text-gray-600 text-sm">
+            {user?.bio || "Bio here"}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightSidebar;
